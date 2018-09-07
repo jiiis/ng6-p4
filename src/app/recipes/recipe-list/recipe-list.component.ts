9e5fae09473c7d49093b7d79efc0c8914f4207d6
Hao Chang <yngwie.chang@gmail.com>
2018-09-08 01:01:12 +1000

    Moved class "Recipe" into the shared "models" folder.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from '../Recipe';
+import { Recipe } from '../../models/Recipe';
 
 @Component({
   selector: 'app-recipe-list',
