fae205e99c29b35619071c08ee0772ec27827dc5
Hao Chang <yngwie.chang@gmail.com>
2018-09-09 00:32:42 +1000

    Moved recipe item HTML code into "RecipeItemComponent" template.

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Recipe } from '../../../models/Recipe';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.scss']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
