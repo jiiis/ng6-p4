6316ef3f650698f5ad06917e7fced313a81cb44f
Hao Chang <yngwie.chang@gmail.com>
2018-10-16 21:23:37 +1100

    Enabled unsubscribing from the ingredients subject in "ShoppingListComponent" to prevent memory leak.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { Ingredient } from '../models/Ingredient';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,7 +9,9 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  private _ingredientsSubscription: Subscription;
+
   ingredients: Ingredient[];
 
   constructor(private _shoppingListService: ShoppingListService) {}
@@ -15,8 +19,12 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
     this.ingredients = this._shoppingListService.ingredients;
 
-    this._shoppingListService.ingredientsSubject.subscribe(
+    this._ingredientsSubscription = this._shoppingListService.ingredientsSubject.subscribe(
       (ingredients: Ingredient[]) => this.ingredients = ingredients
     );
+  }
+
+  ngOnDestroy() {
+    this._ingredientsSubscription.unsubscribe();
   }
 }
